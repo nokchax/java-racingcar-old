f8d817785324b987787948dc79ad24f6ed5b615e
nokchax <nokchax@gmail.com>
2020-05-13 10:30:13 +0900

    test : number class test

--- src/test/java/calculator/expression/NumberTest.java
+++ src/test/java/calculator/expression/NumberTest.java
@@ -2,14 +2,61 @@ package calculator.expression;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("Number 클래스 테스트")
 class NumberTest {
 
     @Test
+    @DisplayName("of 사용으로 객체 생성 확인")
     void constructor() {
+        Expression expression = Number.of("1");
 
+        assertThatCode(() -> Number.of("1"))
+                .doesNotThrowAnyException();
+        assertThat(expression).isNotNull();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("of 객체 생성시 exception 테스트")
+    void ofException(final String exp, final Class<?> exceptionClass) {
+        assertThatThrownBy(() -> Number.of(exp))
+                .isInstanceOf(exceptionClass);
+
+    }
+
+    private static Stream<Arguments> ofException() {
+        return Stream.of(
+                Arguments.of("",IllegalArgumentException.class),
+                Arguments.of(null, IllegalArgumentException.class),
+                Arguments.of("+", NumberFormatException.class)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("테스트를 위한 equals 메소드 테스트")
+    @CsvSource({"-1", "1"})
+    void equals(final String exp) {
+        Number number = Number.of(exp);
+        Number anotherNumber = Number.of(exp);
+
+        assertThat(number).isEqualTo(anotherNumber);
+    }
+
+    @ParameterizedTest(name = "{0} = {1}")
+    @CsvSource({"1,1", "-1,-1"})
+    @DisplayName("수식의 계산이 올바른지")
+    void operate(final String subExp, final int expected) {
+        Number number = Number.of(subExp);
+
+        assertThat(number.operate()).isEqualTo(expected);
     }
 }
