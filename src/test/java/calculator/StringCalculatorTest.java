81253e6d948cc1e37eaf1aff9d867d79c1c5e8af
nokchax <nokchax@gmail.com>
2020-05-13 10:50:35 +0900

    feat : calculator operation test

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,7 +1,6 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -31,4 +30,74 @@ class StringCalculatorTest {
         );
     }
 
+    @MethodSource
+    @DisplayName("뺄셈 테스트")
+    @ParameterizedTest(name = "{0} - {1} = {2}")
+    void sub(int x, int y, int expectedResult) {
+        int result = StringCalculator.calculate(x + " - " + y);
+
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> sub() {
+        return Stream.of(
+                Arguments.of(1, 2, -1),
+                Arguments.of(1, -2, 3),
+                Arguments.of(-1, 2, -3),
+                Arguments.of(-1, -2, 1)
+        );
+    }
+
+    @MethodSource
+    @DisplayName("곱셈 테스트")
+    @ParameterizedTest(name = "{0} * {1} = {2}")
+    void multi(int x, int y, int expectedResult) {
+        int result = StringCalculator.calculate(x + " * " + y);
+
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> multi() {
+        return Stream.of(
+                Arguments.of(1, 2, 2),
+                Arguments.of(1, -2, -2),
+                Arguments.of(-1, 2, -2),
+                Arguments.of(-1, -2, 2)
+        );
+    }
+
+    @MethodSource
+    @DisplayName("나눗셈 테스트")
+    @ParameterizedTest(name = "{0} / {1} = {2}")
+    void divide(int x, int y, int expectedResult) {
+        int result = StringCalculator.calculate(x + " / " + y);
+
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> divide() {
+        return Stream.of(
+                Arguments.of(2, 1, 2),
+                Arguments.of(2, -1, -2),
+                Arguments.of(-2, 1, -2),
+                Arguments.of(-2, -1, 2)
+        );
+    }
+
+    @MethodSource
+    @DisplayName("복잡한 연산 테스트")
+    @ParameterizedTest(name = "{0} = {1}")
+    void complexExp(String exp, int expectedResult) {
+        int result = StringCalculator.calculate(exp);
+
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> complexExp() {
+        return Stream.of(
+                Arguments.of("1+2+3", 6),
+                Arguments.of("1+2*3/3", 3),
+                Arguments.of("-40+-30--20*-10/-5", -100)
+        );
+    }
 }
